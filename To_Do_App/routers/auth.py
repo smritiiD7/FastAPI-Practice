8f808b40cd2aa0f2c9325ee44b384764b093b726
Smriti <smritidubey66@gmail.com>
2025-08-29 19:22:43 +0530

    Added auth.py and ToDo.py under routers folder, added 2 tables for one to many mapping, one user to multiple tasks

--- To_Do_App/routers/auth.py
+++ To_Do_App/routers/auth.py
@@ -1,7 +1,46 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session # type: ignore
+from starlette import status
+#from todos import db_dependency
 
 router = APIRouter()
 
-@router.get("/auth")
-async def get_user():
-    return {'user' : 'authenticated'}
+bcrypt_context=CryptContext(schemes=[ 'bcrypt' ], deprecated='auto')
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+db_dependency = Annotated[Session,Depends(get_db)]
+
+@router.post("/auth",status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request: CreateUserRequest, db: db_dependency):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+       # hashed_password=create_user_request.password, #not hashing, saving as plain text
+        hashed_password= bcrypt_context.hash(create_user_request.password),
+        is_Active=True
+    )
+    db.add(create_user_model)
+    db.commit()
+
+
