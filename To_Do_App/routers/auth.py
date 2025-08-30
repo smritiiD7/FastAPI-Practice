a7a0820ea3b9c15909247829679fbb9bb7bdae15
Smriti <smritidubey66@gmail.com>
2025-08-30 15:31:26 +0530

    feat: add authorization endpoints (/auth and /token) to FastAPI app

--- To_Do_App/routers/auth.py
+++ To_Do_App/routers/auth.py
@@ -6,7 +6,8 @@ from models import Users
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session # type: ignore
 from starlette import status
-#from todos import db_dependency
+from  fastapi.security import OAuth2PasswordRequestForm
+from passlib.context import CryptContext
 
 router = APIRouter()
 
@@ -28,6 +29,14 @@ def get_db():
         db.close()
 db_dependency = Annotated[Session,Depends(get_db)]
 
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:  #if user is none
+        return False
+    if not bcrypt_context.verify( password,user.hashed_password):
+        return False
+    return True
+
 @router.post("/auth",status_code=status.HTTP_201_CREATED)
 async def create_user(create_user_request: CreateUserRequest, db: db_dependency):
     create_user_model = Users(
@@ -43,4 +52,12 @@ async def create_user(create_user_request: CreateUserRequest, db: db_dependency)
     db.add(create_user_model)
     db.commit()
 
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user =  authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    
+
+    return 'Successful Authentication'
 
