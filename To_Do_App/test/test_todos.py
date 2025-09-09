45e2a6009ad0a3d3a734a2d42256dbbc3d9848ac
Smriti <smritidubey66@gmail.com>
2025-09-09 23:54:17 +0530

    Added yaml file

--- To_Do_App/test/test_todos.py
+++ To_Do_App/test/test_todos.py
@@ -1,59 +1,12 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import StaticPool, create_engine, text
-from sqlalchemy.orm import sessionmaker
+
 from to_do_app.routers.todos import get_db, get_current_user
-from to_do_app.database import Base
-from to_do_app.main import app
 from fastapi import status
-import pytest
-from to_do_app.models import ToDos
+from to_do_app.main import app
+from utils import *
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db" #create new database
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args= {"check_same_thread": False},
-    poolclass = StaticPool,
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def override_get_current_user():
-    return {'username' : 'rammy', 'id' : 1,  'user_role' : 'admin' }
 
 app.dependency_overrides[get_db] = override_get_db #Whenever a route depends on get_db, don’t call the real get_db(). Instead, call override_get_db()
 app.dependency_overrides[get_current_user] = override_get_current_user
-
-client = TestClient(app) #creates a fake HTTP client that can talk to your FastAPI app without starting a real server (no uvicorn needed)
-
-@pytest.fixture
-def test_todo():
-    todo = ToDos(
-        title='Learn to Code!',
-        description="Learn Everyday!",
-        priority=5,
-        complete=False,
-        owner_id=1
-    )
-
-    db = TestingSessionLocal()
-    db.add(todo)
-    db.commit()
-    yield todo
-
-    with engine.connect() as connection:
-        connection.execute(text("DELETE FROM todos;"))
-        connection.commit()
-
 
 
 def test_read_all_authenticated(test_todo):
